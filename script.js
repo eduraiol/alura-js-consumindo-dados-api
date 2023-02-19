1ce41232e9bf0033840d4b42b48c55e3686bca27
Eduardo Raiol <marquesraiol@gmail.com>
2023-02-19 18:51:06 -0300

    Preenchendo form com dados retornados da API

--- script.js
+++ script.js
@@ -1,15 +1,47 @@
-const url = 'https://viacep.com.br/ws/66620590/json/';
+let logradouro = document.getElementById('endereco');
+let bairro = document.getElementById('bairro');
+let cidade = document.getElementById('cidade');
+let estado = document.getElementById('estado');
 
-const consultaCep = fetch(url)
-.then(response => response.json())
-.then(data => {
-    if(data.erro){
-        throw Error('Esse CEP não existe!');
-    }else{
-        console.log(data)
+let mensagemErro = document.getElementById('erro');
+mensagemErro.innerHTML = "";
+
+async function buscaEndereco(cep) {
+
+    try{
+        const url = `https://viacep.com.br/ws/${cep}/json/`;
+        const consultaCep = await fetch(url);
+        const consultaCepConvertida = await consultaCep.json();
+        if(consultaCepConvertida.erro){
+            throw Error('CEP não existente!');
+        }
+
+        logradouro.value = consultaCepConvertida.logradouro;
+        bairro.value = consultaCepConvertida.bairro;
+        cidade.value = consultaCepConvertida.localidade;
+        estado.value = consultaCepConvertida.uf;
+        //console.log(consultaCepConvertida);
+        return consultaCepConvertida;
+    }catch(erro){
+        limpaCampos();
+        mensagemErro.innerHTML = `<p> CEP inválido. Tente novamente </p>`
+        //console.log(erro);
     }
- })
-.catch(erro => console.log(erro))
-.finally(mensagem => console.log('Processamento concluído!'));
 
-//console.log(consultaCep);
+}
+
+let cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
+
+function limpaCampos(){
+    logradouro.value = '';
+    bairro.value = '';
+    cidade.value = '';
+    estado.value = '';
+}
+
+
+// let ceps = ['66635902', '66620590', '66080007'];
+// let conjuntoCeps = ceps.map(valor => buscaEndereco(valor));
+// Promise.all(conjuntoCeps)
+//     .then(respostas => console.log(respostas));
